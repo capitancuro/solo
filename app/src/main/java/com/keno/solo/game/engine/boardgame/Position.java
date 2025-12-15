699bc859cc05d99c7a105cc074f370a7f55fa093
curo <kenocuro@gmail.com>
2025-12-15 14:55:00 -0800

    feat: Position and Unit Implementation This commit implements workable versions of class Position and class Unit, that can be used to implement boardgames.

--- app/src/main/java/com/keno/solo/game/engine/boardgame/Position.java
+++ app/src/main/java/com/keno/solo/game/engine/boardgame/Position.java
@@ -1,5 +1,34 @@
 package com.keno.solo.game.engine.boardgame;
 
-public class Position {
-    
+import com.keno.solo.game.engine.boardgame.Unit;
+
+public class Position 
+{
+    private int x, y;
+    private Unit unit;
+
+    public Position(int x, int y, Unit unit)
+    {
+        setUnit(unit);
+    }
+
+    public void setUnit(Unit unit)
+    {
+        this.unit = unit;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public Unit getUnit()
+    {
+        return unit;
+    }
 }
--- app/src/main/java/com/keno/solo/game/engine/boardgame/Unit.java
+++ app/src/main/java/com/keno/solo/game/engine/boardgame/Unit.java
@@ -1,5 +1,24 @@
 package com.keno.solo.game.engine.boardgame;
 
-public class Unit {
-    
+import com.keno.solo.game.engine.boardgame.Position;
+
+public class Unit 
+{
+    private int value;
+    private Position position;
+
+    public Unit(int value, Position position)
+    {
+        setPosition(position);
+    }
+
+    public void setPosition(Position position)
+    {
+        this.position = position;
+    }
+
+    public Position getPosition()
+    {
+        return position;
+    }
 }
