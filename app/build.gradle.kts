7c4392b04199c74280a0a675db30637a94598cba
curo <kenocuro@gmail.com>
2025-12-14 19:17:20 -0800

    chore: Added OpenJFX Gradle
    Add OpenJFX among Gradle Plugins to enable the
    use of the use of JavaFX for UI development.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,8 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+    
+    id("org.openjfx.javafxplugin") version "0.0.14" //OpenJFX Grade Plugin (Latest)
 }
 
 repositories {
