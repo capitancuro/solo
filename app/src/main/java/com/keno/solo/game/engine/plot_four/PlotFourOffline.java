6f28ae485cfd77eda3bb8832448ca4791dd98ac9
curo <kenocuro@gmail.com>
2025-12-15 16:46:54 -0800

    feat: PlotFourOffline Constructor This commit implements a portion of PlotFourOffline's constructor, and implements non-functional versions setUnits() and setBoard()

--- app/src/main/java/com/keno/solo/game/engine/plot_four/PlotFourOffline.java
+++ app/src/main/java/com/keno/solo/game/engine/plot_four/PlotFourOffline.java
@@ -1,6 +1,32 @@
 package com.keno.solo.game.engine.plot_four;
 
+import java.util.List;
+
 public class PlotFourOffline extends PlotFour
 {
-    
+    public PlotFourOffline()
+    {
+        super
+        (
+            "Plot Four Offline", 
+            "Play plot four offline! \n Take turns using shared peripherals \n or connect using bluetooth",
+            List.of("boardgame", "tabletop", "strategy")
+        );
+    }
+
+    public void setUnits()
+    {
+
+    }
+
+    public  void setBoard(int x, int y)
+
+    {
+
+    }
+
+    public void play()
+    {
+
+    }
 }
