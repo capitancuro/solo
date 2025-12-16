a58e8ea44ba6600152738c6adb26cd4ee8c87987
curo <kenocuro@gmail.com>
2025-12-15 16:21:57 -0800

    feat: Implement Plot Four Constructor This commit implements the constructor for public abstract class PlotFour.

--- app/src/main/java/com/keno/solo/game/engine/plot_four/PlotFour.java
+++ app/src/main/java/com/keno/solo/game/engine/plot_four/PlotFour.java
@@ -1,6 +1,26 @@
 package com.keno.solo.game.engine.plot_four;
 
-public abstract class PlotFour 
+import java.util.List;
+
+import com.keno.solo.game.engine.boardgame.Boardgame;
+
+public abstract class PlotFour extends Boardgame
 {
-    
+    protected PlotFour
+    (
+        String name, 
+        String description, 
+        List<String> genres
+    )
+    {
+        super
+        (
+            name, 
+            "E", 
+            "playable", 
+            description, 
+            genres, 
+            0
+        );
+    }
 }
