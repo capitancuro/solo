1778353c39d668aa02069c40f7793d6b4a7efed5
curo <kenocuro@gmail.com>
2025-12-16 11:37:12 -0800

    chore: Scanner Test Tests project configuration through creation of Scanner object and retrieval of user input through terminal through that object.

--- app/src/main/java/com/keno/solo/App.java
+++ app/src/main/java/com/keno/solo/App.java
@@ -1,9 +1,14 @@
 package com.keno.solo;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main(String[] args) 
     { 
-            System.out.println("Hello, Solo Application!");
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("Input number!");
+       int col = scanner.nextInt();
+       System.out.println("Number is " + col);
     }
 }
