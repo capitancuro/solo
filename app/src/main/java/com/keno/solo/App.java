007199ca5eedf3c1dd6ca3b8543110af45414fad
curo <kenocuro@gmail.com>
2025-12-16 21:23:04 -0800

    chore: Test Move Tests the move function in PlotFourOffline

--- app/src/main/java/com/keno/solo/App.java
+++ app/src/main/java/com/keno/solo/App.java
@@ -2,13 +2,35 @@ package com.keno.solo;
 
 import java.util.Scanner;
 
+import com.keno.solo.game.engine.plot_four.PlotFourOffline;
+
 public class App 
 {
     public static void main(String[] args) 
     { 
-       Scanner scanner = new Scanner(System.in);
-       System.out.println("Input number!");
-       int col = scanner.nextInt();
-       System.out.println("Number is " + col);
+        PlotFourOffline off = new PlotFourOffline();
+        Scanner scanner = new Scanner(System.in);
+        int col = 0;
+        while(col != -1)
+        {
+                for(int r = 0; r < 6; r++)
+                {
+                    for(int c = 0; c < 7; c++)
+                    {
+                        System.out.print(off.getBoard().get(r).get(c).getUnit() == null? "[ ]" : "[1]");
+                    }
+                    System.out.println();
+                }
+
+                System.out.println("Input column");
+                col = scanner.nextInt();
+
+                if(col > -1 && col < 7)
+                {
+                    off.move(col);
+                }
+
+        }
+        scanner.close();
     }
 }
