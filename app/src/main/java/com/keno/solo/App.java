facbebfa3afa29bb3eff9f95b1587834fe29cfcd
curo <kenocuro@gmail.com>
2026-01-01 15:12:27 -0800

    refactor: C/C++ Adoption

--- app/src/main/java/com/keno/solo/App.java
+++
@@ -1,8 +0,0 @@
-package com.keno.solo;
-
-public class App 
-{
-    public static void main(String[] args) 
-    { 
-    }
-}
--- app/src/main/java/com/keno/solo/game_logic/boardgame/plot_four/PlotFourPiece.java
+++
@@ -1,3 +0,0 @@
-package com.keno.solo.game_logic.boardgame.plot_four;
-
-public enum PlotFourPiece{RED, YELLOW};
