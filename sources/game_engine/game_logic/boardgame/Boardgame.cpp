8213d66de22f635bdae49af89d81fa6db1554c76
curo <kenocuro@gmail.com>
2026-01-07 15:03:32 -0800

    feat: Boardgame Base Class Implementation

---
+++ sources/game_engine/game_logic/boardgame/Boardgame.cpp
@@ -0,0 +1,29 @@
+#pragma once
+#include "Boardgame.hpp"
+
+template <typename BoardPiece>
+Boardgame<BoardPiece>::Boardgame
+(
+    std::string name,
+    std::string rating,
+    std::string type,
+    std::string description,
+    std::vector<std::string> genres,
+    int price
+) : Game
+(
+    name, 
+    rating, 
+    type, 
+    description, 
+    genres, 
+    price
+)
+{
+}
+
+template <typename BoardPiece>
+std::vector<std::vector<BoardPosition<BoardPiece>>> Boardgame<BoardPiece>::getBoard() const 
+{
+    return board;
+}
